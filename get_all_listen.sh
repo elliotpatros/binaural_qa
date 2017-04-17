037c3422a03327eff45a87823c2dc0639fdb0800
Elliot Patros <empatros@gmail.com>
2017-04-16 19:23:34 -0700

    no changes. just tabs to spaces.

--- get_all_listen.sh
+++ get_all_listen.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+
 for i in {1002..1060}
 do
     IF="ftp://ftp.ircam.fr/pub/IRCAM/equipes/salles/listen/archive/SUBJECTS/IRC_$i.zip"
     OF="./HRTFs/IRC_$i.zip"
-	echo "downloading $IF to $OF"
+    echo "downloading $IF to $OF"
     curl $IF --progress-bar --output $OF
-	if [ -f $OF ]
-	then 
-		echo "expanding $OF"
-		unzip -q $OF -d "./HRTFs/IRC_$i/"
-		rm $OF
-	else
-		echo "couldn't find $IF"
-	fi
+    if [ -f $OF ]
+    then 
+        echo "expanding $OF"
+        unzip -q $OF -d "./HRTFs/IRC_$i/"
+        rm $OF
+    else
+        echo "couldn't find $IF"
+    fi
 done
